da51643b02b2fa04523f206f59b3293ee67ccabd
Treasure-07 <treasure.nforlem@dawsoncollege.qc.ca>
2024-09-12 20:45:55 +0000

    Activity 3

---
+++ cake.py
@@ -0,0 +1,22 @@
+import math
+
+def getCylinderVolume(radius, height) :
+    volume = math.pi * (radius ** 2) * height
+    return volume 
+
+x = getCylinderVolume(10,12)
+y = getCylinderVolume(2, 6)
+print(round(x,4))
+print(round(y,4))
+print(int(x))
+
+def getNumberOfSlices(radius, height, volumeOfSlice) :
+    volume = getCylinderVolume(radius, height)
+    numberOfslices = volume / volumeOfSlice
+    return int(numberOfslices)
+
+numberOfSlices1 = getNumberOfSlices(10, 10, 100)
+print(numberOfSlices1)
+
+
+    
